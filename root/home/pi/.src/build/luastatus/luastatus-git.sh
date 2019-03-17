8e4a3d2b8f6a82f0af502fd3379461a99583977d
Alex <mateone4you@gmail.com>
2019-03-17 19:54:38 +0500

    edit root/home/pi/.src/build/luastatus/luastatus-git.sh

--- root/home/pi/.src/build/luastatus/luastatus-git.sh
+++ root/home/pi/.src/build/luastatus/luastatus-git.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+
+# luastatus is a universal status bar content generator
+# depends=('lua' 'libx11' 'bash' 'alsa-lib' 'yajl>=2.1.0' 'xcb-util-wm')
+# sudo pacman -S lua libx11 bash alsa-lib yajl xcb-util-wm
+
 git clone https://github.com/shdown/luastatus.git &&
 cd ./luastatus &&
 cmake . && make && sudo make install &&
