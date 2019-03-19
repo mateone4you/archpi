07bb43b59f6e73d87328fbf3c8152d47f3b7fee6
Alex <48623775+mateone4you@users.noreply.github.com>
2019-03-19 21:32:07 +0500

    Update config-6.2.h

--- root/home/pi/.src/build/dwm/config-6.2.h
+++ root/home/pi/.src/build/dwm/config-6.2.h
@@ -11,11 +11,11 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#000000";		# цвет фона
+static const char col_gray2[]       = "#FFFFFF";		# цвет границ активного окна
+static const char col_gray3[]       = "#bbbbbb";		# цвет текста в статус баре
+static const char col_gray4[]       = "#eeeeee";		# цвет текста заголовка окна
+static const char col_cyan[]        = "#005577";		# цвет фона заголовка окна
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
