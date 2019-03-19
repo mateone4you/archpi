864f564805ed523bb4da6ab35364faababeaf961
Alex <48623775+mateone4you@users.noreply.github.com>
2019-03-19 21:58:37 +0500

    Update config-6.2.h

--- root/home/pi/.src/build/dwm/config-6.2.h
+++ root/home/pi/.src/build/dwm/config-6.2.h
@@ -12,10 +12,10 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#000000";
-static const char col_gray2[]       = "#FF0000";
+static const char col_gray2[]       = "#005577";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#FF0000";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
