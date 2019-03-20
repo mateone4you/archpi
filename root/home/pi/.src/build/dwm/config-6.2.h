73b69b027179eafbccc36f9a98ac6c1ebc0cd76f
Alex <mateone4you@gmail.com>
2019-03-20 12:01:44 +0500

    add Roboto Mono ttf

--- root/home/pi/.src/build/dwm/config-6.2.h
+++ root/home/pi/.src/build/dwm/config-6.2.h
@@ -9,8 +9,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "Roboto Mono Medium:size=10" };
+static const char dmenufont[]       = "Roboto Mono Light:size=10";
 static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#005577";
 static const char col_gray3[]       = "#bbbbbb";
@@ -133,7 +133,7 @@ static Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-/*	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },*/
+	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
@@ -141,6 +141,6 @@ static Button buttons[] = {
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
-	{ ClkStatusText,        0,              Button3,        spawn,          {.v = mymenucmd } },	
+	{ ClkStatusText,        0,              Button1,        spawn,          {.v = mymenucmd } },	
 };
 
--- root/home/pi/.src/build/st/st-git.sh
+++ root/home/pi/.src/build/st/st-git.sh
@@ -4,7 +4,7 @@ cd st
 patch -p1 < ../patches/st-alpha-0.8.2.diff &&
 patch -p1 < ../patches/st-anysize-0.8.1.diff &&
 patch -p1 < ../patches/st-clipboard-0.8.2.diff &&
-cp ../config*.h config.h &&
+cp ../config-0.8.2.h config.h &&
 make &&
 sudo make install
 make clean
